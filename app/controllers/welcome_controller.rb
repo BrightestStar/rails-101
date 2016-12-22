431c06d72071b2c165ed1dc78c2f50df3887232a
“将叫我将军” <d2194499406@gmail.com>
2016-12-22 20:05:39 +0800

    create groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "元帅，如果没有吩咐，那臣就退下了？"
+    flash[:alert] = "这里是将军府，元帅有事您吩咐！"
+    flash[:warning] = "我就在这里，随时恭侯大驾！"
+  end
 end
