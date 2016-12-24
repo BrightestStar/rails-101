522dcbd618203660e9c645fcad59f8a3c507dc81
“将叫我将军” <d2194499406@gmail.com>
2016-12-24 18:53:03 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -45,6 +45,32 @@ class GroupsController < ApplicationController
     redirect_to groups_path, alert: "乱扔垃圾可不好！"
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+     if !current_user.is_member_of?(@group)
+       current_user.join!(@group)
+       flash[:notice] = "士兵你来了，一边候着吧。"
+     else
+       flash[:warning] = "你认为将军是那么好当的吗？好好努力吧，士兵！"
+     end
+
+     redirect_to group_path(@group)
+   end
+
+    def quit
+      @group = Group.find(params[:id])
+
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert] = "你认为这是你家后花院吗？想来就来想走就走。"
+      else
+        flash[:warning] = "你不是本营士兵.再进一步杀！！！"
+      end
+
+      redirect_to group_path(@group)
+    end
+
   private
 
   def find_group_and_check_permission
@@ -52,7 +78,7 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
 
     if current_user != @group.user
-      redirect_to root_path, alert: "这是谁的地盘你不知道吗？看清楚再进入来."
+      redirect_to root_path, alert: "这是谁的地盘你不知道吗？看清楚了再进。"
     end
   end
 
